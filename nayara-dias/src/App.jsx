549208183b7d2f170085267b4f72b1c0165403eb
nayaradias1 <nayara_dias95@hotmail.com>
2024-02-26 11:43:00 -0300

    first commit

---
+++ nayara-dias/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Nayara Dias</h1>
+      <h2>Seja bem vindo a mais um desafio</h2>
+    </>
+  )
+}
+
+export default App
